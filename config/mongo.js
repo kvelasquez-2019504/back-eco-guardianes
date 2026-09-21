0bf56a6d43f99f1c5cccb1b5cccc34182a75c61f
Kenneth Velasquez <kvelasquez-2019504@kinal.edu.gt>
2026-09-21 01:46:44 -0600

    fix: conexion for mongo+srv

--- config/mongo.js
+++ config/mongo.js
@@ -1,6 +1,15 @@
 'use strict';
 
+import dns from 'dns';
 import mongoose from 'mongoose';
+
+// Resolver bloqueo de registros DNS SRV (querySrv ECONNREFUSED) de routers/ISPs locales para MongoDB Atlas
+try {
+    dns.setServers([
+        process.env.DNS_1, process.env.DNS_2]);
+} catch (e) {
+    // Mantener DNS del sistema en caso de entornos restringidos
+}
 
 export const connectionDB = async () => {
     const { connection, connect } = mongoose;
